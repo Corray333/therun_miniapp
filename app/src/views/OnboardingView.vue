b6518233c0ba9612ed283b42013e8946bb075bc7
Corray333 <mark.corray.off@gmail.com>
2024-09-17 23:20:11 +0300

    add onboarding

--- app/src/views/OnboardingView.vue
+++ app/src/views/OnboardingView.vue
@@ -1,57 +1,93 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import {useRouter} from 'vue-router'
 
 const { t } = useI18n()
+const router = useRouter()
 
-const currentSlide = ref<number>(1)
+const currentSlide = ref<number>(0)
 
 
 </script>
 
 <template>
     <section>
-        <Transition name="slide">
-            <div v-if="currentSlide == 1" class="slide">
-                <div class="pic-container">
-                    <div class="pic" id="onboarding-pic-1"></div>
+        <div class="slider">
+            <div class="slide-track" :style="`transform:translateX(${-100*currentSlide}vw)`">
+                <div class="slide">
+                    <div class="pic-container">
+                        <div class="pic" id="onboarding-pic-1"></div>
+                    </div>
+                    <div class="info">
+                        <h1>{{ t('screens.onboarding.slide1.title') }}</h1>
+                        <p>{{ t('screens.onboarding.slide1.description') }}</p>
+                        <button @click="currentSlide = 1">{{ t('screens.onboarding.nextButton') }}</button>
+                    </div>
                 </div>
-                <div class="info">
-                    <h1>{{ t('screens.onboarding.slide1.title') }}</h1>
-                    <p>{{ t('screens.onboarding.slide1.description') }}</p>
-                    <button @click="currentSlide+=2">Next</button>
+                <div class="slide">
+                    <div class="pic-container">
+                        <div class="pic" id="onboarding-pic-3"></div>
+                    </div>
+                    <div class="info">
+                        <h1>{{ t('screens.onboarding.slide3.title') }}</h1>
+                        <p>{{ t('screens.onboarding.slide3.description') }}</p>
+                        <button @click="currentSlide = 2">{{ t('screens.onboarding.nextButton') }}</button>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="pic-container">
+                        <div class="pic bg-bottom" id="onboarding-pic-4"></div>
+                    </div>
+                    <div class="info">
+                        <h1>{{ t('screens.onboarding.slide4.title') }}</h1>
+                        <p>{{ t('screens.onboarding.slide4.description') }}</p>
+                        <button @click="currentSlide = 3">{{ t('screens.onboarding.nextButton') }}</button>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="pic-container">
+                        <div class="pic" id="onboarding-pic-5"></div>
+                    </div>
+                    <div class="info">
+                        <h1>{{ t('screens.onboarding.slide5.title') }}</h1>
+                        <p>{{ t('screens.onboarding.slide5.description') }}</p>
+                        <button @click="router.push('/farm')">{{ t('screens.onboarding.startButton') }}</button>
+                    </div>
                 </div>
             </div>
-            <div v-if="currentSlide == 3" class="slide">
-                <div class="pic-container">
-                    <div class="pic" id="onboarding-pic-3"></div>
-                </div>
-                <div class="info">
-                    <h1>{{ t('screens.onboarding.slide3.title') }}</h1>
-                    <p>{{ t('screens.onboarding.slide3.description') }}</p>
-                    <button @click="currentSlide++">Next</button>
-                </div>
-            </div>
-        </Transition>
+        </div>
     </section>
 </template>
 
 
 <style>
+.slider {
+    overflow: hidden;
+}
+
+.slide-track {
+    display: flex;
+    transition: transform 0.5s; 
+}
+
 .slide {
     width: 100vw;
     height: 100vh;
     display: flex;
     flex-direction: column;
+    flex: 0 0 auto;
 }
-.pic-container{
+
+.pic-container {
     height: 100%;
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.pic{
+
+.pic {
     height: 100%;
     width: 100%;
     max-width: 80%;
@@ -59,7 +95,8 @@ const currentSlide = ref<number>(1)
     background-repeat: no-repeat;
     background-size: contain;
 }
-.info{
+
+.info {
     padding: 1rem;
     display: flex;
     flex-direction: column;
@@ -69,32 +106,34 @@ const currentSlide = ref<number>(1)
     box-shadow: 0 -0.25rem 1rem 0 rgba(0, 0, 0, 0.1);
 }
 
-#onboarding-pic-1{
+#onboarding-pic-1 {
     background-image: url('../assets/images/onboarding/slide1.png');
 }
-#onboarding-pic-2{
+
+#onboarding-pic-2 {
     background-image: url('../assets/images/onboarding/slide2.png');
 }
-#onboarding-pic-3{
+
+#onboarding-pic-3 {
     background-image: url('../assets/images/onboarding/slide3.png');
 }
-#onboarding-pic-4{
+
+#onboarding-pic-4 {
     background-image: url('../assets/images/onboarding/slide4.png');
 }
-#onboarding-pic-5{
+
+#onboarding-pic-5 {
     background-image: url('../assets/images/onboarding/slide5.png');
 }
 
 
-.slide-left-enter-active,
-.slide-right-leave-active {
-    transition: all 0.3s ease;
+.slide-enter-active,
+.slide-leave-active {
+    transition: transform 0.5s;
 }
 
-.slide-left-enter,
-.slide-left-leave-to,
-.slide-right-enter,
-.slide-right-leave-to {
+.slide-enter,
+.slide-leave-to {
     transform: translateX(100%);
 }
 </style>
